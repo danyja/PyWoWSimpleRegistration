35c95bae0b40de5b84b1c1d0a5518fc7aaec2168
danyja <danyja@163.com>
2025-07-30 12:16:17 +0800

    update code

--- wow/models/characters.py
+++ wow/models/characters.py
@@ -1,14 +1,15 @@
 from wow import db
-from datetime import datetime
+
+
 class Characters(db.Model):
-    __tablename__ = 'characters'
-    __bind_key__ = 'characters'
-    
+    __tablename__ = "characters"
+    __bind_key__ = "characters"
+
     guid = db.Column(db.Integer, primary_key=True, nullable=False)
     account = db.Column(db.Integer, nullable=False)
-    name = db.Column(db.String(12), unique=True, nullable=False, default='')
+    name = db.Column(db.String(12), unique=True, nullable=False, default="")
     race = db.Column(db.Integer, nullable=False)
-    class_ = db.Column('class', db.Integer, nullable=False)
+    class_ = db.Column("class", db.Integer, nullable=False)
     gender = db.Column(db.Integer, nullable=False)
     level = db.Column(db.Integer, nullable=False)
     xp = db.Column(db.Integer, nullable=False)
@@ -16,4 +17,3 @@ class Characters(db.Model):
     online = db.Column(db.Integer, nullable=False, default=0)
     totaltime = db.Column(db.Integer, nullable=False, default=0)
     logout_time = db.Column(db.Integer)
-    
